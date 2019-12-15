1f0d5c622dd31c74f66675d0899c7cfb329f924d
Andreas Reich <r_andreas2@web.de>
2019-12-15 23:10:45 +0100

    rendering anti-aliased lines with Kiss <3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate kiss3d;
+extern crate nalgebra as na;
+
+use kiss3d::window::*;
+use na::{Point3};
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = Window::new_with_setup(
+        "lorenz-rs",
+        1920,
+        1080,
+        CanvasSetup {
+            vsync: true,
+            samples: NumSamples::Sixteen,
+        },
+    );
+    window.set_background_color(0.1, 0.1, 0.15);
+
+    while window.render() {
+        let a = Point3::new(-0.1, -0.1, 0.0);
+        let b = Point3::new(0.0, 0.1, 0.0);
+        let c = Point3::new(0.1, -0.1, 0.0);
+
+        window.draw_line(&a, &b, &Point3::new(1.0, 0.0, 0.0));
+        window.draw_line(&b, &c, &Point3::new(0.0, 1.0, 0.0));
+        window.draw_line(&c, &a, &Point3::new(0.0, 0.0, 1.0));
+    }
 }
