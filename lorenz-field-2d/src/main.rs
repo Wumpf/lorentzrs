b2a9dd9ca2d2577a8c2dc40fe78b6922ea21e6d9
Andreas Reich <r_andreas2@web.de>
2019-12-23 13:37:00 +0100

    bounding box for 2d thingy

--- lorenz-field-2d/src/main.rs
+++ lorenz-field-2d/src/main.rs
@@ -1,15 +1,13 @@
 extern crate minifb;
 extern crate nalgebra as na;
+extern crate num_traits;
 
 use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
 use na::{clamp, Point3, Vector3};
+use num_traits::bounds::Bounded;
 
 fn lorenz_eq(p: Point3<f64>, sigma: f64, beta: f64, rho: f64) -> Vector3<f64> {
-    Vector3::new(
-        sigma * (p[1] - p[0]),
-        p[0] * (rho - p[2]) - p[1],
-        p[0] * p[1] - beta * p[2],
-    )
+    Vector3::new(sigma * (p[1] - p[0]), p[0] * (rho - p[2]) - p[1], p[0] * p[1] - beta * p[2])
 }
 
 fn rk4step<F: Fn(Point3<f64>) -> Vector3<f64>>(p: Point3<f64>, h: f64, f: F) -> Vector3<f64> {
@@ -20,6 +18,24 @@ fn rk4step<F: Fn(Point3<f64>) -> Vector3<f64>>(p: Point3<f64>, h: f64, f: F) -> 
     let k4 = h * f(p + k3);
 
     (k1 + 2.0 * (k2 + k3) + k4) / 6.0
+}
+
+struct BoundingBox {
+    min: Point3<f64>,
+    max: Point3<f64>,
+}
+
+fn compute_boundingbox<'a, T: Iterator<Item = &'a Point3<f64>>>(points: T) -> BoundingBox {
+    points.fold(
+        BoundingBox {
+            min: Point3::max_value(),
+            max: Point3::min_value(),
+        },
+        |aabb, p| BoundingBox {
+            min: Point3::new(aabb.min.x.min(p.x), aabb.min.y.min(p.y), aabb.min.z.min(p.z)),
+            max: Point3::new(aabb.max.x.max(p.x), aabb.max.y.max(p.y), aabb.max.z.max(p.z)),
+        },
+    )
 }
 
 const WIDTH: usize = 100;
@@ -63,6 +79,10 @@ fn main() {
 
         buffer[i] = (clamp(points[i].x / 15.0 + 1.0, 0.0, 1.0) * 255.0) as u32;
     }
+
+    let bb = compute_boundingbox(points.iter());
+    println!("min point: {:?}", bb.min);
+    println!("max point: {:?}", bb.max);
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
