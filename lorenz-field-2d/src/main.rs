c59a1fd9c3e9bb7c47373bbca2a4ffd72d13de25
Andreas Reich <r_andreas2@web.de>
2019-12-25 09:29:19 +0100

    normalized rgb picture

--- lorenz-field-2d/src/main.rs
+++ lorenz-field-2d/src/main.rs
@@ -38,20 +38,27 @@ fn compute_boundingbox<'a, T: Iterator<Item = &'a Point3<f64>>>(points: T) -> Bo
     )
 }
 
-const WIDTH: usize = 100;
-const HEIGHT: usize = 100;
+fn pack_rgb(r: f64, g: f64, b: f64) -> u32 {
+    let ri = (clamp(r, 0.0, 1.0) * 255.0) as u32;
+    let gi = (clamp(g, 0.0, 1.0) * 255.0) as u32;
+    let bi = (clamp(b, 0.0, 1.0) * 255.0) as u32;
+    bi + (gi << 8) + (ri << 16)
+}
+
+const WIDTH: usize = 600;
+const HEIGHT: usize = 600;
 
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT * 2];
-    let mut points: Vec<Point3<f64>> = vec![Point3::new(0.0, 0.0, 0.0); WIDTH * HEIGHT * 2];
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut points: Vec<Point3<f64>> = vec![Point3::new(0.0, 0.0, 0.0); WIDTH * HEIGHT];
 
     let mut window = Window::new(
         "Lorenz thing - ESC to exit",
         WIDTH,
         HEIGHT,
         WindowOptions {
-            resize: true,
-            scale: Scale::X2,
+            resize: false,
+            scale: Scale::X1,
             scale_mode: ScaleMode::AspectRatioStretch,
             ..WindowOptions::default()
         },
@@ -62,13 +69,13 @@ fn main() {
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
     window.set_background_color(0, 0, 0);
 
-    let num_steps = 100;
+    let num_steps = 30;
     let step_size = 0.1;
     let sigma = 10.0;
     let beta = 8.0 / 3.0;
     let rho = 28.0;
 
-    for i in 0..buffer.len() {
+    for i in 0..points.len() {
         let x = (i % WIDTH) as f64 / (WIDTH - 1) as f64;
         let y = (i / HEIGHT) as f64 / (HEIGHT - 1) as f64;
         points[i] = Point3::new(x * 30.0 - 15.0, 1.0, y * 30.0);
@@ -76,13 +83,18 @@ fn main() {
             let step = rk4step(points[i], step_size, |p| lorenz_eq(p, sigma, beta, rho));
             points[i] += step;
         }
-
-        buffer[i] = (clamp(points[i].x / 15.0 + 1.0, 0.0, 1.0) * 255.0) as u32;
     }
 
     let bb = compute_boundingbox(points.iter());
     println!("min point: {:?}", bb.min);
     println!("max point: {:?}", bb.max);
+
+    for i in 0..buffer.len() {
+        let r = (points[i].x - bb.min.x) / (bb.max.x - bb.min.x);
+        let g = (points[i].y - bb.min.y) / (bb.max.y - bb.min.y);
+        let b = (points[i].z - bb.min.z) / (bb.max.z - bb.min.z);
+        buffer[i] = pack_rgb(r,g,b);
+    }
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
